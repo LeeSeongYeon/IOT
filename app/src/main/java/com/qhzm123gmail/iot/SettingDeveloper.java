a4e615b98775a7591204dd559ad2ab15e2c0d228
MAC <mac@MACui-MacBook-Pro.local>
2017-05-11 00:37:35 +0900

    Initial commit

---
+++ app/src/main/java/com/qhzm123gmail/iot/SettingDeveloper.java
@@ -0,0 +1,17 @@
+package com.qhzm123gmail.iot;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by mac on 2017. 5. 10..
+ */
+
+public class SettingDeveloper extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.developer_fragment);
+    }
+}
