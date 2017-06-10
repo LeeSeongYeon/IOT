bb98b9174b5cb14277941416153c45697ca1ca0a
qhzm1 <qhzm123@gmail.com>
2017-06-11 02:21:50 +0900

    2017-06-11 02:20 블루투스 요청

---
+++ app/src/main/java/com/qhzm123gmail/iot/Bluetooth.java
@@ -0,0 +1,44 @@
+package com.qhzm123gmail.iot;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
+
+/**
+ * Created by qhzm1 on 2017-06-11.
+ */
+
+public class Bluetooth {
+    private static final String TAG = "BlutoothClass";
+    private static final int BLUETOOTH_OFF = 100;
+
+    private BluetoothAdapter bluetoothAdapter;
+
+    private Activity mainActivty;
+    private Handler handler;
+
+    public Bluetooth(Activity activity, Handler han) {
+        mainActivty = activity;
+        handler = han;
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            Toast.makeText(mainActivty, "@string/bluetooth_null", Toast.LENGTH_SHORT).show();
+            ActivityCompat.finishAffinity(mainActivty);
+        }
+        else {
+            Toast.makeText(mainActivty, "Hello", Toast.LENGTH_SHORT).show();
+        }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent intend = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            mainActivty.startActivityForResult(intend, BLUETOOTH_OFF);
+        }
+    }
+
+
+
+}
